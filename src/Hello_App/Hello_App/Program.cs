13221b9686b5e4f20f00eab88b98916f7b8c74c2
Yashaswini Gowda <Yashaswini.Gowda@oneadvanced.com>
2018-03-15 16:33:42 +0530

    Added files again

---
+++ src/Hello_App/Hello_App/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!!!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/Welcome_App/Welcome_App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Welcome_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome!!!");
+            Console.ReadLine();
+        }
+    }
+}
